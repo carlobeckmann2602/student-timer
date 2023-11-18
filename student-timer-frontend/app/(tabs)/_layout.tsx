26524a8559c673bc07fe11022c80229d0785a58e
Konstantin Haupt <konstantin.haupt@yahoo.de>
2023-11-18 13:57:30 +0100

    Revert "refactor: wrap App-layout in SafeAreaView"
    This reverts commit 5352747013734cd56acc60c7383a946cc762bc8f.

--- student-timer-frontend/app/(tabs)/_layout.tsx
+++ student-timer-frontend/app/(tabs)/_layout.tsx
@@ -5,58 +5,53 @@ import { Pressable, useColorScheme } from "react-native";
 import { COLORTHEME } from "@/constants/Theme";
 import Header from "@/components/Header";
 import { BarChart2, LayoutList, TimerReset, User2 } from "lucide-react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabLayout() {
   // const colorScheme = useColorScheme();
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: COLORTHEME.light.primary,
-          tabBarInactiveTintColor: COLORTHEME.light.tabIconDefault,
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: COLORTHEME.light.primary,
+        tabBarInactiveTintColor: COLORTHEME.light.tabIconDefault,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Tracking",
+          tabBarIcon: ({ color }) => (
+            <TimerReset name="clock-o" color={color} />
+          ),
+          header: () => <Header title="Tracking" />,
         }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Tracking",
-            tabBarIcon: ({ color }) => (
-              <TimerReset name="clock-o" color={color} />
-            ),
-            header: () => <Header title="Tracking" />,
-          }}
-        />
-        <Tabs.Screen
-          name="modules"
-          options={{
-            title: "Module",
-            tabBarIcon: ({ color }) => (
-              <LayoutList name="module" color={color} />
-            ),
-            header: () => <Header title="Module" />,
-          }}
-        />
-        <Tabs.Screen
-          name="statistics"
-          options={{
-            title: "Statistik",
-            tabBarIcon: ({ color }) => (
-              <BarChart2 name="statistic" color={color} />
-            ),
-            header: () => <Header title="Statistik" />,
-          }}
-        />
-        <Tabs.Screen
-          name="profil"
-          options={{
-            title: "Profil",
-            tabBarIcon: ({ color }) => <User2 name="profil" color={color} />,
-            header: () => <Header title="Module" />,
-          }}
-        />
-      </Tabs>
-    </SafeAreaView>
+      />
+      <Tabs.Screen
+        name="modules"
+        options={{
+          title: "Module",
+          tabBarIcon: ({ color }) => <LayoutList name="module" color={color} />,
+          header: () => <Header title="Module" />,
+        }}
+      />
+      <Tabs.Screen
+        name="statistics"
+        options={{
+          title: "Statistik",
+          tabBarIcon: ({ color }) => (
+            <BarChart2 name="statistic" color={color} />
+          ),
+          header: () => <Header title="Statistik" />,
+        }}
+      />
+      <Tabs.Screen
+        name="profil"
+        options={{
+          title: "Profil",
+          tabBarIcon: ({ color }) => <User2 name="profil" color={color} />,
+          header: () => <Header title="Module" />,
+        }}
+      />
+    </Tabs>
   );
 }
