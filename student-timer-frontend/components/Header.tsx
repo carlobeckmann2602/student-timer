4aee809ffb90e0082bf1cb35aa74377a83cb4157
Konstantin Haupt <konstantin.haupt@yahoo.de>
2023-11-18 12:52:32 +0100

    Refactor: Adjust Header to new Text-Components & add padding

--- student-timer-frontend/components/Header.tsx
+++ student-timer-frontend/components/Header.tsx
@@ -1,20 +1,22 @@
 import { StyleSheet } from "react-native";
 
-import { Text, View } from "./Themed";
+import { View } from "./Themed";
+import { H1 } from "./StyledText";
 
 export default function Header(props: { title: string }) {
   const { title } = props;
   return (
-    <View>
-      <Text style={styles.header}>{title}</Text>
+    <View style={styles.headerContainer}>
+      <H1>{title}</H1>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    fontSize: 32,
-    fontWeight: "600",
-    textAlign: "center",
+  headerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 24,
+    paddingBottom: 12,
   },
 });
