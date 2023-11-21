ca18e722538256da671cd7db536a5729b521c8b5
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2023-11-21 23:25:54 +0100

    added profile

--- student-timer-frontend/app/(tabs)/profil.tsx
+++ student-timer-frontend/app/(tabs)/profil.tsx
@@ -1,33 +1,10 @@
-import { StyleSheet } from "react-native";
+import { Text, View } from "@/components/Themed";
+import Profile from "@/components/profile/profile";
 
-import { Text, View } from "../../components/Themed";
-
-export default function TabTwoScreen() {
+export default function LoginScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profil</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+    <View>
+      <Profile />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
