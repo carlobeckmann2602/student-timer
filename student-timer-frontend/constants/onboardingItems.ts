2c3d8d629ecd103fc1a2e917ffa35ff6511deb1a
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2023-11-29 00:29:51 +0100

    #36 onboarding workaround: switched from svg to png

--- student-timer-frontend/constants/onboardingItems.ts
+++ student-timer-frontend/constants/onboardingItems.ts
@@ -2,21 +2,21 @@ export const onboardingData = [
     {
         title: 'Tracking',
         description: 'Tracke ganz einfach deine Lernzeit.',
-        image: require('@/assets/images/onboarding/tracking.svg'),
+        image: require('@/assets/images/onboarding/tracking_vergleich.png'),
     },
     {
         title: 'Modulverwaltung',
         description: 'Erstelle Module und verwalte sie.',
-        image: require('@/assets/images/onboarding/modulverwaltung.svg'),
+        image: require('@/assets/images/onboarding/modulverwaltung.png'),
     },
     {
         title: 'Erfolg',
         description: 'Schaue dir deine Erfolge direkt in der App an.',
-        image: require('@/assets/images/onboarding/erfolg.svg'),
+        image: require('@/assets/images/onboarding/erfolg.png'),
     },
     {
         title: 'Vergleich',
         description: 'Vergleiche die Arbeitszeit für deine Module.',
-        image: require('@/assets/images/onboarding/vergleich.svg'),
+        image: require('@/assets/images/onboarding/tracking_vergleich.png'),
     },
 ];
