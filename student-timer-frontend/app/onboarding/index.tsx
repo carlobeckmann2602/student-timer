31e2134a921d88e378a32dd10d5657aadc923778
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2023-12-04 21:28:35 +0100

    #36 skip-button exchanged for Link

--- student-timer-frontend/app/onboarding/index.tsx
+++ student-timer-frontend/app/onboarding/index.tsx
@@ -1,60 +1,56 @@
 // OnboardingScreen.tsx bzw. index.tsx
 import React, { useState } from "react";
-import { View } from "@/components/Themed";
-import Button from "@/components/Button";
-import { COLORTHEME } from "@/constants/Theme";
+import { View, Text } from "@/components/Themed";
 import {onboardingData} from "../../constants/onboardingItems";
 import OnboardingCards from "../../components/onboarding/OnboardingCards";
 import CardNavigation from "../../components/onboarding/CardNavigation";
 import Header from "../../components/Header";
-import {router} from "expo-router";
+import {Link, router} from "expo-router";
 import {StyleSheet} from "react-native";
 
 export default function OnboardingScreen() {
-  const [activeIndex, setActiveIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
 
-  const navigateToAuthentication = () => {
-    router.push("/(auth)/signup");
-  };
+    const navigateToAuthentication = () => {
+        router.push("/signup");
+    };
 
-  const onPrevPress = () => {
-    setActiveIndex((prevIndex) => Math.max(prevIndex - 1, 0));
-  };
-  const onNextPress = () => {
-    if (activeIndex < onboardingData.length - 1) {
-      setActiveIndex((prevIndex) => prevIndex + 1);
-    } else {
-      navigateToAuthentication();
-    }
-  };
+    const onPrevPress = () => {
+        setActiveIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+    };
+    const onNextPress = () => {
+        if (activeIndex < onboardingData.length - 1) {
+        setActiveIndex((prevIndex) => prevIndex + 1);
+        } else {
+        navigateToAuthentication();
+        }
+    };
 
-  return (
-      <View style={styles.container}>
-        <View>
-          <Header title="StudentTimer" />
-        </View>
-        <OnboardingCards
-            onboardingData={onboardingData}
-            activeIndex={activeIndex}
-        />
-        <CardNavigation
-            cardAmount={onboardingData.length}
-            activeIndex={activeIndex}
-            onPrevPress={onPrevPress}
-            onNextPress={onNextPress}
-        />
-        <Button
-            text="Überspringen"
-            backgroundColor={COLORTHEME.light.primary}
-            textColor="#FFFFFF"
-            onPress={navigateToAuthentication}
-            style={{ width: 300, marginVertical: 20, height: 50 }}
-        />
+    return (
+        <View style={styles.container}>
+            <View>
+                <Header title="StudentTimer" />
+            </View>
+            <OnboardingCards
+                onboardingData={onboardingData}
+                activeIndex={activeIndex}
+            />
+            <CardNavigation
+                cardAmount={onboardingData.length}
+                activeIndex={activeIndex}
+                onPrevPress={onPrevPress}
+                onNextPress={onNextPress}
+            />
+            <Text style={{marginTop: 15}}>
+              <Link href="/signup" alt="Überspringen zur Registrierung" style={{ textDecorationLine: "underline"}}>
+                  Überspringen
+              </Link>
+            </Text>
       </View>
   );
 }
 
-//toDo styling für iPhone SE anpassenfpo
+//toDo styling für iPhone SE anpassen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
