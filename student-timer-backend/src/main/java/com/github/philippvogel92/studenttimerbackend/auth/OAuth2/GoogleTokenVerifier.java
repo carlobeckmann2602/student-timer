eef9862de435f503d881778ef5f279c59251d05c
Philipp <philipp242@live.de>
2023-12-09 12:42:53 +0100

    refactor: set googleClientId in .env

--- student-timer-backend/src/main/java/com/github/philippvogel92/studenttimerbackend/auth/OAuth2/GoogleTokenVerifier.java
+++ student-timer-backend/src/main/java/com/github/philippvogel92/studenttimerbackend/auth/OAuth2/GoogleTokenVerifier.java
@@ -1,27 +1,27 @@
-package com.github.philippvogel92.studenttimerbackend.auth.oAuth2;
+package com.github.philippvogel92.studenttimerbackend.auth.OAuth2;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.apache.v2.ApacheHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 
 @Component
 public class GoogleTokenVerifier {
 
     private final GoogleIdTokenVerifier verifier;
+    @Value("${googleClientId}")
+    private String googleClientId;
 
     public GoogleTokenVerifier() {
         this.verifier = new GoogleIdTokenVerifier.Builder(new ApacheHttpTransport(), new GsonFactory())
                 // Specify the CLIENT_ID of the app that accesses the backend:
-                .setAudience(Collections.singletonList("698641812741-oobavbktgq9n5ursb8qd88kou1vc6pkn.apps" +
-                        ".googleusercontent.com"))
+                .setAudience(Collections.singletonList(googleClientId))
                 .build();
     }
 
