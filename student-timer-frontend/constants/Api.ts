77622c4fbf63692c18e600fd24447326aaf059ea
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2023-12-27 23:20:00 +0100

    feat: set api url to ip of pc in dev mode to enable backend access with real smartphone

--- student-timer-frontend/constants/Api.ts
+++ student-timer-frontend/constants/Api.ts
@@ -1,3 +1,11 @@
+import Constants from "expo-constants";
 import { Platform } from "react-native";
-export const API_URL = "https://student-timer-backend.onrender.com";
+
+export const API_URL = __DEV__
+  ? `http://${
+      Platform.OS === "web"
+        ? "localhost"
+        : Constants.expoConfig?.hostUri?.split(":").shift() || "localhost"
+    }:8080`
+  : "https://student-timer-backend.onrender.com";
 export const TOKEN_KEY = "jwt-token";
