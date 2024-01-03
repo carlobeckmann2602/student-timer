e4283d66dfff16965ebfe392dc55e2da2b2c2010
Konstantin Haupt <konstantin.haupt@yahoo.de>
2024-01-03 09:26:32 +0100

    feature: Add colorpicker for choosing colorcode of new module

--- student-timer-frontend/app/(tabs)/modules/new/index.tsx
+++ student-timer-frontend/app/(tabs)/modules/new/index.tsx
@@ -1,14 +1,30 @@
 import Button from "@/components/Button";
 import DateTimePicker from "@/components/DateTimePicker";
 import InputField from "@/components/InputField";
+import { P } from "@/components/StyledText";
 import { View } from "@/components/Themed";
 import { COLORTHEME } from "@/constants/Theme";
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 export default function NewModule() {
   const router = useRouter();
+  const selectableColors: string[] = [
+    "#88A795",
+    "#AB5761",
+    "#5D7CB9",
+    "#FBC2B5",
+    "#073B3A",
+    "#243119",
+    "#FA7921",
+    "#88A7F5",
+  ];
 
   const [moduleName, setModuleName] = useState("");
   const [examDate, setExamDate] = useState<Date>(new Date());
@@ -97,11 +113,34 @@ export default function NewModule() {
           />
         </View>
         <View style={styles.row}>
-          <InputField
-            label="Farbauswahl*"
-            onChangeText={setColorCode}
-            value={colorCode}
-          />
+          <View style={styles.colorWrapper}>
+            <P style={styles.inputLabelText}>Farbauswahl*</P>
+            <View style={styles.colorContainer}>
+              {selectableColors.map((color) => {
+                return (
+                  <TouchableOpacity
+                    style={styles.colorOptionWrapper}
+                    onPress={() => setColorCode(color)}
+                  >
+                    <View
+                      style={[
+                        styles.colorOptionIndicator,
+                        {
+                          borderColor:
+                            colorCode === color
+                              ? COLORTHEME.light.primary
+                              : "transparent",
+                        },
+                      ]}
+                    />
+                    <View
+                      style={[styles.colorOption, { backgroundColor: color }]}
+                    />
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          </View>
           <View style={{ width: "70%", backgroundColor: "transparent" }} />
         </View>
       </View>
@@ -156,5 +195,48 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     gap: 15,
+  },
+  colorWrapper: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    width: "100%",
+    backgroundColor: "transparent",
+    gap: 4,
+  },
+  inputLabelText: {
+    color: COLORTHEME.light.primary,
+  },
+  colorContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "100%",
+    justifyContent: "space-between",
+    gap: 20,
+    alignItems: "center",
+    alignContent: "center",
+    backgroundColor: "transparent",
+  },
+  colorOptionWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 60,
+    height: 60,
+  },
+  colorOptionIndicator: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -26 }, { translateY: -26 }],
+    width: 52,
+    height: 52,
+    backgroundColor: "transparent",
+    borderRadius: 1000,
+    borderWidth: 4,
+  },
+  colorOption: {
+    width: 40,
+    height: 40,
+    borderRadius: 1000,
   },
 });
