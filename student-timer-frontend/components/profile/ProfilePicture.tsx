daaf2ba48ff5a5561e9a18eea51d6ecde9faf8af
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-02-07 23:25:08 +0100

    #141 Use of Icon as defaultPicture instead of jpg

--- student-timer-frontend/components/profile/ProfilePicture.tsx
+++ student-timer-frontend/components/profile/ProfilePicture.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Pressable, StyleSheet, View } from "react-native";
+import { Image, Pressable,View } from "react-native";
 import { User2 } from "lucide-react-native";
 import { Edit2 } from "lucide-react-native";
 import { COLORTHEME } from "@/constants/Theme";
@@ -31,19 +31,23 @@ export default function ProfilePicture({
     : "Aktuelles Profilbild";
   const accessibilityRole = onPress ? "button" : "image";
 
-  const imageWidth = miniature ? 24 : 120;
+  const imageSize = miniature ? 24 : 120;
+  const iconSize = miniature ? 22 : 112;
+  const containerBackgroundColor = miniature ? COLORTHEME.light.background : COLORTHEME.light.grey1;
+
 
   return (
-    <View style={[
-        styles.profileImageContainer,
-        {
-          width: imageWidth,
-          height: imageWidth,
-          borderRadius: imageWidth / 2,
-          borderWidth: imageWidth / 24,
-          borderColor: color
-        },
-    ]}>
+    <View style={{
+        justifyContent: "center",
+        alignItems: "center",
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2,
+        borderWidth: imageSize / 24,
+        borderColor: color,
+        backgroundColor: containerBackgroundColor,
+      }}
+    >
       <Pressable
         onPress={onPress}
         accessibilityLabel={accessibilityLabel}
@@ -52,35 +56,45 @@ export default function ProfilePicture({
         {imagePath ? (
           <Image
             source={imagePath}
-            style={{ width: imageWidth, height: imageWidth, borderRadius: imageWidth / 2, borderWidth: imageWidth/24, borderColor: color }}
+            style={{ 
+              width: imageSize, 
+              height: imageSize, 
+              borderRadius: imageSize / 2, 
+              borderWidth: imageSize / 24, 
+              borderColor: color 
+          }}
             accessibilityLabel={"Aktuelles Profilbild"}
           />
         ) : (
-          <User2 size={100} color={COLORTHEME.light.primary} />
+          <View style={{
+            width: imageSize,
+            height: imageSize,
+            borderRadius: imageSize / 2,
+            overflow: 'hidden',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <User2 size={iconSize} color={color} />
+          </View>
         )}
         {editStyle && (
-          <View style={styles.editIcon}>
-            <Edit2 size={24} color={COLORTHEME.light.background} />
+          <View style={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            borderRadius: 60,
+            backgroundColor: COLORTHEME.light.primary,
+            borderColor: COLORTHEME.light.primary,
+            borderWidth: 6,
+            }}
+          >
+            <Edit2 
+                size={24} 
+                color={COLORTHEME.light.background} 
+            />
           </View>
         )}
       </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  profileImageContainer: {
-    backgroundColor: "#EEEAEA",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  editIcon: {
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    borderRadius: 60,
-    backgroundColor: COLORTHEME.light.primary,
-    borderColor: COLORTHEME.light.primary,
-    borderWidth: 6,
-  },
-});
