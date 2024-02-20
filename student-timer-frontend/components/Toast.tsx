bf57747c53eff714f11072535010cd04de97b9ae
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2024-02-20 10:40:24 +0100

    fix: increase toast elevation, caused weird style when toast disappeared

--- student-timer-frontend/components/Toast.tsx
+++ student-timer-frontend/components/Toast.tsx
@@ -109,7 +109,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.27,
     shadowRadius: 4.65,
-    elevation: 6,
+    elevation: 50,
   },
   textContainer: {
     flexDirection: "column",
