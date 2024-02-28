1b37082c0240d3bb38e5c9379db10ca93283cd1b
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2024-02-28 17:43:23 +0100

    fix: show push notification on lock screen for android

--- student-timer-frontend/libs/handleLocalNotification.ts
+++ student-timer-frontend/libs/handleLocalNotification.ts
@@ -30,7 +30,8 @@ const registerForPushNotificationsAsync = async () => {
       name: "default",
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FFAABBCC",
+      lightColor: "#FFFFFF",
+      lockscreenVisibility: Notifications.AndroidNotificationVisibility.PUBLIC,
     });
   }
 
